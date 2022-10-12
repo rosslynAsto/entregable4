7f50f1308c0162ad052850de728c1d3785598972
RosslynAsto <rosly_13_5@hotmail.com>
2022-10-12 10:32:45 -0500

    12-10-ok

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import FormUsers from './components/FormUsers'
 import UserCard from './components/UserCard'
 
 
-const baseURL = 'http://144.126.218.162:9000/swagger'
+const baseURL = 'http://144.126.218.162:9000'
 
 function App() {
 
